087ea41508bc744ed849cd9a6d60553b5a4e015c
Felipe Lourenco <felipe@countrhq.com>
2021-03-24 14:24:04 +0100

    Deploy to netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,14 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    // New plugin
+    new HtmlWebpackPlugin({
+      // injects bundle.js to our new index.html
+      inject: true,
+      // copys the content of the existing index.html to the new /build index.html
+      template: path.resolve('./index.html')
+    })
+  ]
 }
